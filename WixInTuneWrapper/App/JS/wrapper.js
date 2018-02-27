31429b0a1108bc4df28b6a3950f9a071ab14d158
Rasq <g.gorol@gmail.com>
2018-02-27 21:25:36 +0100

    first ui version
    still incomplete

--- WixInTuneWrapper/App/JS/wrapper.js
+++ WixInTuneWrapper/App/JS/wrapper.js
@@ -23,77 +23,106 @@ $('.ui.loading .form').submit((e)=>{
 });
 
 
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
 $('.start-button').click(function(){
-  startApp();
+  startApp('.Opcja01-sidebar');
 });
+//----------------------------------------------------------------------------------------------------------------------
 
-
+//----------------------------------------------------------------------------------------------------------------------
 $('.Opcja01-sidebar').click(function(){
-  console.dir('Opcja01');
-  $(this).parent('.sidebar').find('.active').removeClass('active green');
-  $(this).addClass('active green');
-  HideAll(1);
+  HideAll(this, '.ui.Opcja01');
 });
 
 
 $('.Opcja02-sidebar').click(function(){
-  console.dir('Opcja02');
-  $(this).parent('.sidebar').find('.active').removeClass('active green');
-  $(this).addClass('active green');
-  HideAll(2);
+  HideAll(this, '.ui.Opcja02');
 });
 
 
 $('.Opcja03-sidebar').click(function(){
-  console.dir('Opcja03');
-  $(this).parent('.sidebar').find('.active').removeClass('active green');
-  $(this).addClass('active green');
-  HideAll(3);
+  HideAll(this, '.ui.Opcja03');
 });
 
 
 $('.Opcja04-sidebar').click(function(){
-  console.dir('Opcja04');
-  $(this).parent('.sidebar').find('.active').removeClass('active green');
-  $(this).addClass('active green');
-  HideAll(4);
+  HideAll(this, '.ui.Opcja04');
 });
 
 
+$('.Opcja05-sidebar').click(function(){
+  HideAll(this, '.ui.Opcja05');
+});
+//----------------------------------------------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------------------------------------------
-function startApp(){
+$('.ui.checkbox.left.A').click(function(){
+  CheckBoxMSIActions(!$(this).checkbox('is checked'), 'CB_CA');
+});
+
+$('.ui.checkbox.left.B').click(function(){
+  CheckBoxMSIActions(!$(this).checkbox('is checked'), 'CB_LC');
+});
+
+$('.ui.checkbox.left.C').click(function(){
+  CheckBoxMSIActions(!$(this).checkbox('is checked'), 'CB_Shortcuts');
+});
+
+$('.ui.checkbox.left.D').click(function(){
+  CheckBoxMSIActions(!$(this).checkbox('is checked'), 'CB_Reg');
+});
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+function startApp(initTab){
     $('.starter').fadeOut('fast',function(){
       $('.ui.dashboard').fadeIn('fast');
     });
 
-
-    HideAll(1);
+    HideAll(initTab, '.ui.Opcja01');
+    HideAllCheckBoxMSI();
 }
 //----------------------------------------------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------------------------------------------
-function HideAll(optionNumber){
+function HideAll(optionName, tabName){
     $('.ui.Opcja01').fadeOut('fast');
     $('.ui.Opcja02').fadeOut('fast');
     $('.ui.Opcja03').fadeOut('fast');
     $('.ui.Opcja04').fadeOut('fast');
 
-    switch(optionNumber) {
-      case 1:
-        $('.ui.Opcja01').fadeIn('fast');
-      break;
-      case 2:
-        $('.ui.Opcja02').fadeIn('fast');
-      break;
-      case 3:
-        $('.ui.Opcja03').fadeIn('fast');
-      break;
-      case 4:
-        $('.ui.Opcja04').fadeIn('fast');
-      break;
-      default:
-        $('.ui.Opcja01').fadeIn('fast');
-      }
+    $(tabName).fadeIn('fast');
+
+    $(optionName).parent('.sidebar').find('.active').removeClass('active green');
+    $(optionName).addClass('active green');
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+function HideAllCheckBoxMSI(){
+    $('.ui.checkbox.left.A').checkbox('set unchecked');
+    $('.ui.checkbox.left.B').checkbox('set unchecked');
+    $('.ui.checkbox.left.C').checkbox('set unchecked');
+    $('.ui.checkbox.left.D').checkbox('set unchecked');
+
+    $('.CB_CA').fadeOut('fast');
+    $('.CB_LC').fadeOut('fast');
+    $('.CB_Shortcuts').fadeOut('fast');
+    $('.CB_Reg').fadeOut('fast');
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+function CheckBoxMSIActions(state, action){
+  if (state == true) {
+    $('.' + action).fadeIn('fast');
+  } else {
+    $('.' + action).fadeOut('fast');
+  }
 }
 //----------------------------------------------------------------------------------------------------------------------
